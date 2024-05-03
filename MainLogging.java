ce50f35bfaf5ac97ff8c1586bf517846128eb020
Kevin Gräßler <kevin.graessler@cgi.com>
2024-05-03 23:03:26 +0200

    Added JavaDoc Comments

--- MainLogging.java
+++ MainLogging.java
@@ -3,14 +3,31 @@ import javax.lang.model.element.Element;
 import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
-import javax.swing.text.Document;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-
 public class MainLogging{
+/**
+ * Constructs and displays a login window using Java Swing.
+ * 
+ * This constructor sets up a simple graphical user interface (GUI) for user login,
+ * consisting of a JFrame containing fields for username and password input,
+ * and a login button to submit the credentials. When the login button is clicked,
+ * it checks the entered credentials against those stored in an XML file.
+ * If the credentials are valid, a confirmation dialog is shown; otherwise,
+ * an error message is displayed.
+ *
+ * The layout is manually set, and the JFrame is sized at 200x200 pixels. The GUI includes:
+ * - A JLabel and a JTextField for the username.
+ * - A JLabel and a JPasswordField for the password.
+ * - A JButton that triggers the authentication process.
+ *
+ * The JFrame is set to exit the application on close. All components are positioned using absolute coordinates.
+ *
+ * @author Nova177
+ */
   public MainLogging() {
     JFrame frame = new JFrame("Login");
     frame.setSize(200, 200);  
@@ -61,8 +78,28 @@ public class MainLogging{
     frame.setLayout(null);
     frame.setVisible(true);
   }
+  /**
+ * Checks user credentials against an XML file to verify if the provided username
+ * and password match any user entry in the file.
+ *
+ * This method reads from a predefined XML file located at a specified path on the disk.
+ * It searches through each "user" element in the XML to find a matching username and password.
+ * If a match is found, the method returns true, indicating successful authentication.
+ * If no match is found or if any error occurs during the process (such as file not found or XML parsing errors),
+ * it returns false.
+ *
+ * @param username the username entered by the user, to be verified against the XML.
+ * @param password the password entered by the user, to be verified against the XML.
+ * @return true if the username and password match an entry in the XML file; false otherwise.
+ * @throws Exception if there is any issue accessing or parsing the XML file. Specific errors such as
+ *         file not found or XML parsing errors are caught and handled within the method,
+ *         with relevant error messages displayed to the user.
+ * @author Nova177
+ */
   private boolean checkUserCredentialsinXML(String username, String password) {
-    File inputFile = new File("C:/Users/kevin.graessler/Desktop/LoginWindowJava/userlist.xml");
+    String path = System.getProperty("user.dir");
+    String fileName = "userList.xml";
+    File inputFile = new File(path+"\\"+fileName);
     try {
         if (!inputFile.exists()) {
             JOptionPane.showMessageDialog(null, "XML file not found at the specified path.");
@@ -71,16 +108,16 @@ public class MainLogging{
 
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = (Document) dBuilder.parse(inputFile);
-        ((org.w3c.dom.Document) doc).getDocumentElement().normalize();
+        org.w3c.dom.Document doc = dBuilder.parse(inputFile);  // Directly use as org.w3c.dom.Document
+        doc.getDocumentElement().normalize();
 
-        NodeList nList = ((org.w3c.dom.Document) doc).getElementsByTagName("user");
+        NodeList nList = doc.getElementsByTagName("user");
         for (int i = 0; i < nList.getLength(); i++) {
             Node nNode = nList.item(i);
             if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                 Element eElement = (Element) nNode;
-                String xmlUsername = ((org.w3c.dom.Document) eElement).getElementsByTagName("username").item(0).getTextContent();
-                String xmlPassword = ((org.w3c.dom.Document) eElement).getElementsByTagName("password").item(0).getTextContent();
+                String xmlUsername = ((org.w3c.dom.Document) eElement).getElementsByTagName("username").item(0).getTextContent();  // Correctly access 'username'
+                String xmlPassword = ((org.w3c.dom.Document) eElement).getElementsByTagName("password").item(0).getTextContent();  // Correctly access 'password'
                 if (xmlUsername.equals(username) && xmlPassword.equals(password)) {
                     return true;
                 }
@@ -93,3 +130,7 @@ public class MainLogging{
     return false;
 }
 }
+
+
+
+
